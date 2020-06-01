afbefa939cda6f8ac5e7ef547fdd026a244e06a9
kongfanwu <kongfan_wu@qq.com>
2020-06-01 11:12:55 +0800

    add

---
+++ sdmm/lib/public/GlobalConfig.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/*
+ *系统主题设置，包括系统默认字体 背景色等
+ */
+class GlobalConfig {
+  static bool isDebug = true;//是否是调试模式
+  static final signString = 'U7doak7fl4da45d';
+  static final signString1 = 'U7doak7fl4da45d';
+
+//  static bool dark = false;
+//  static Color fontColor = Colors.black54;
+}
